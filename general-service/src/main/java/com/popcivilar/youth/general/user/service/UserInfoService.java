11d2b632b79d604709c6cbf3a16bfe5d0851df0f
popcivilar <zcwdgz100700@163.com>
2019-07-02 12:35:59 +0800

    修改类

--- general-service/src/main/java/com/popcivilar/youth/general/user/service/UserInfoService.java
+++ general-service/src/main/java/com/popcivilar/youth/general/user/service/UserInfoService.java
@@ -3,7 +3,6 @@ package com.popcivilar.youth.general.user.service;
 import com.popcivilar.youth.general.user.dao.UserInfoMapper;
 import com.popcivilar.youth.general.user.entity.UserInfo;
 import com.popcivilar.youth.youthbase.base.BaseMapper;
-import com.popcivilar.youth.youthbase.base.BaseService;
 import com.popcivilar.youth.youthbase.base.BaseServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -13,7 +12,13 @@ import java.util.Date;
 
 @Service
 @Transactional
-public class UserInfoService{
+public class UserInfoService {
 
+    @Autowired
+    private BaseMapper<UserInfo> mapper;
 
+    public String sayHello (){
+        return mapper.selectByPrimaryKey(1).toString();
+
+    }
 }
