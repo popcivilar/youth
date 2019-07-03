28657cacbb43c1e342d67a55e2bb62a74dd73fef
popcivilar <zcwdgz100700@163.com>
2019-07-03 22:49:35 +0800

    修改类

--- general-service/src/main/java/com/popcivilar/youth/general/user/web/UserController.java
+++ general-service/src/main/java/com/popcivilar/youth/general/user/web/UserController.java
@@ -6,6 +6,8 @@ import com.popcivilar.youth.general.user.service.UserInfoService;
 import com.popcivilar.youth.youthbase.base.controller.BaseController;
 import com.popcivilar.youth.youthbase.base.entity.ModuleReturn;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -22,10 +24,14 @@ public class UserController extends BaseController<UserInfo,UserInfoService> {
     @Autowired
     private UserInfoService userInfoService;
 
+    @Autowired
+    private RedisTemplate redisTemplate;
+
     @GetMapping("/getInfo")
     public ModuleReturn getInfo(){
         userInfoService.sayHello();
         UserInfo userInfo = userInfoService.selectByPrimaryKey("1");
+        redisTemplate.opsForValue().set("user",userInfo);
         ModuleReturn<UserInfo> success = ModuleReturn.success(userInfo);
         return success;
     }
