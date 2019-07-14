407701fc165ef10bbe3208561da681439f8d0b1e
popcivilar <zcwdgz100700@163.com>
2019-07-15 00:45:10 +0800

    修改类

--- general-service/src/main/java/com/popcivilar/youth/general/article/service/impl/ArticleInfoServiceImpl.java
+++ general-service/src/main/java/com/popcivilar/youth/general/article/service/impl/ArticleInfoServiceImpl.java
@@ -1,12 +1,17 @@
 package com.popcivilar.youth.general.article.service.impl;
 
+import com.popcivilar.youth.general.article.dao.ArticleInfoMapper;
 import com.popcivilar.youth.general.article.entity.ArticleInfo;
 import com.popcivilar.youth.general.article.service.ArticleInfoService;
 import com.popcivilar.youth.youthbase.base.service.BaseServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @Transactional
 public class ArticleInfoServiceImpl extends BaseServiceImpl<ArticleInfo> implements ArticleInfoService {
+
+    @Autowired
+    private ArticleInfoMapper articleInfoMapper;
 }
